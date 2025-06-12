2f88003080a0eed4669db492653c6c8f8d6c97de
smile <2391327789@qq.com>
2025-06-12 08:38:45 +0800

    Revert "update hello.py"
    This reverts commit 1c925d8c13202453aa3bcc77d747742187a4d6a5.

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 def main():
+<<<<<<< HEAD
     print("hello,python!update version 6_12！")
+=======
+    print("hello,python")
+>>>>>>> parent of 1c925d8 (update hello.py)
 
 
 if __name__ == "__main__":
