cf18502b975d212f55808763681bf741d63def88
smile <2391327789@qq.com>
2025-06-12 08:44:57 +0800

    最新的version

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
 def main():
-<<<<<<< HEAD
-    print("hello,python!update version 6_12！")
-=======
     print("hello,python")
->>>>>>> parent of 1c925d8 (update hello.py)
-
-
 if __name__ == "__main__":
     main()
